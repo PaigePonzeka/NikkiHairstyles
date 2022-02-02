0ebb2721143a9621f57e1ff8f2706fdbc54f169c
Paige Ponzeka <pponzeka@yext.com>
2022-02-01 19:23:58 -0800

    Updating Copy right and adding new Work Page

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,26 @@
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
+
+window.onload = function() {
+
+    initGallery();
+};
+
+function initGallery() {
+
+  var $grid = $('.js-masonry-grid-container').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 100,
+    percentPosition: true
+  });
+
+  // layout Masonry after each image loads
+  /*$grid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+  });*/
+
+  $grid.on( 'click', '.grid-item', function() {
+    $(this).toggleClass('gigante');
+    // trigger layout after item size changes
+    $grid.masonry('layout');
+  });
+  }
